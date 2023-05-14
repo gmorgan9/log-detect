bb100cd3fad74f5794a50eb1a9cc0878bf834c55
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-05-13 18:42:41 -0600

    update

---
+++ html/assets/scripts/insert_alert.js
@@ -0,0 +1,18 @@
+function postData() {
+    fetch('https://ld-api.morganserver.com/insert_alert', {
+      method: 'POST',
+    })
+    .then(response => {
+      if (response.ok) {
+        // Handle successful response
+        console.log('Alerts inserted into the database');
+      } else {
+        // Handle error response
+        console.error('Error inserting alerts into the database');
+      }
+    })
+    .catch(error => {
+      // Handle fetch error
+      console.error('Error:', error);
+    });
+  }
