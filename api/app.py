a9f83d83162f5ec6ae8eaa51340f2f43a1ca5d6f
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-05-15 07:07:13 -0600

    update

--- api/app.py
+++ api/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, redirect, session, render_template
+from flask import Flask, request, jsonify, redirect, session, require
 import psycopg2
 from flask_session import Session
 from flask_cors import CORS
