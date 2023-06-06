b29c6b087a8fbbd0e708a3be350e4fed0fb94fba
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-06-06 16:05:06 -0600

    update

--- html/assets/scripts/script.js
+++ html/assets/scripts/script.js
@@ -1,24 +1,4 @@
-fetch('https://ld-api.morganserver.com/api/data')
-  .then(response => response.json())
-  .then(data => {
-    var user = data[0];
-    var userMode = user.mode;
 
-    console.log(userMode);
-    if (userMode === 1) {
-      // User mode is 1
-      console.log("dark");
-    } else if (userMode === 2) {
-      // User mode is 2
-      console.log("light");
-    } else {
-      // User mode is neither 1 nor 2
-      console.log("nothing");
-    }
-  })
-  .catch(error => {
-    console.error('Error:', error);
-  });
 
 var sunButton = document.getElementById('sunButton');
 var moonButton = document.getElementById('moonButton');
