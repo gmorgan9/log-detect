bd6a062900ef7d14034fd1251881f7e9cc7f9c5a
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-06-06 15:50:37 -0600

    update

--- html/assets/scripts/script.js
+++ html/assets/scripts/script.js
@@ -122,7 +122,27 @@ var sun1 = document.getElementById('sun1');
 var moon = document.getElementById('moon');
 var graph = document.getElementById('graph');
 
+fetch('https://ld-api.morganserver.com/api/data')
+  .then(response => response.json())
+  .then(data => {
+    var user = data[0];
+    var userMode = user.mode;
 
+    console.log(userMode);
+    if (userMode === 1) {
+      // User mode is 1
+      console.log("dark");
+    } else if (userMode === 2) {
+      // User mode is 2
+      console.log("light");
+    } else {
+      // User mode is neither 1 nor 2
+      console.log("nothing");
+    }
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
 
 sunButton.addEventListener('click', function() {
   setLightTheme();
