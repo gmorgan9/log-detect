4686c614d427a385cc126ec77a4d1443d1cdf213
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-06-06 16:02:09 -0600

    update

--- html/assets/scripts/script.js
+++ html/assets/scripts/script.js
@@ -1,4 +1,3 @@
-document.addEventListener('DOMContentLoaded', function() {
 fetch('https://ld-api.morganserver.com/api/data')
   .then(response => response.json())
   .then(data => {
@@ -20,7 +19,6 @@ fetch('https://ld-api.morganserver.com/api/data')
   .catch(error => {
     console.error('Error:', error);
   });
-});
 
 var sunButton = document.getElementById('sunButton');
 var moonButton = document.getElementById('moonButton');
