74fe31383faf0636141b017564547139a2d71ad8
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-06-06 16:16:03 -0600

    update

--- html/assets/scripts/mode.js
+++ html/assets/scripts/mode.js
@@ -1,79 +1,79 @@
-var sunButton = document.getElementById('sunButton');
-var moonButton = document.getElementById('moonButton');
-var sun1 = document.getElementById('sun1');
-var moon = document.getElementById('moon');
-var graph = document.getElementById('graph');
+// var sunButton = document.getElementById('sunButton');
+// var moonButton = document.getElementById('moonButton');
+// var sun1 = document.getElementById('sun1');
+// var moon = document.getElementById('moon');
+// var graph = document.getElementById('graph');
 
-function setLightTheme() {
+// function setLightTheme() {
     
-  }
+//   }
 
-  function setDarkTheme() {
+//   function setDarkTheme() {
     
-  }
+//   }
 
   
 
-fetch('https://ld-api.morganserver.com/api/data')
-  .then(response => response.json())
-  .then(data => {
-    var user = data[0];
-    var userMode = user.mode;
+// fetch('https://ld-api.morganserver.com/api/data')
+//   .then(response => response.json())
+//   .then(data => {
+//     var user = data[0];
+//     var userMode = user.mode;
 
-    var htmlElement = document.querySelector('html[data-bs-theme]');
-  var currentTheme = htmlElement.dataset.bsTheme;
-  var paperBgColor;
-  var fontColor;
-    console.log(userMode);
-    if (userMode === 1) {
-      // User mode is 1
-      var htmlElement = document.querySelector('html[data-bs-theme]');
-      htmlElement.dataset.bsTheme = 'dark';
-      moon.classList.add('hide');
-      moon.classList.remove('active');
-      sun1.classList.remove('hide');
-      sun1.classList.add('active');
-      graph.classList.add('border1');
-      console.log("dark");
-      paperBgColor = 'dark';
-      fontColor = '#aeb5bc';
-    } else if (userMode === 2) {
-      // User mode is 2
-      var htmlElement = document.querySelector('html[data-bs-theme]');
-      htmlElement.dataset.bsTheme = 'light';
-      moon.classList.remove('hide');
-      moon.classList.add('active');
-      sun1.classList.add('hide');
-      sun1.classList.remove('active');
-      graph.classList.add('border');
-      console.log("light");
-      paperBgColor = 'light';
-      fontColor = '#515151';
-    } else {
-      // User mode is neither 1 nor 2
-      console.log("nothing");
-    }
-    if (currentTheme !== paperBgColor) {
-        // Change the theme only if it's different from the user's preference
-        htmlElement.dataset.bsTheme = paperBgColor;
-      }
-    updateGraphColors(paperBgColor, fontColor);
-  })
-  .catch(error => {
-    console.error('Error:', error);
-  });
+//     var htmlElement = document.querySelector('html[data-bs-theme]');
+//   var currentTheme = htmlElement.dataset.bsTheme;
+//   var paperBgColor;
+//   var fontColor;
+//     console.log(userMode);
+//     if (userMode === 1) {
+//       // User mode is 1
+//       var htmlElement = document.querySelector('html[data-bs-theme]');
+//       htmlElement.dataset.bsTheme = 'dark';
+//       moon.classList.add('hide');
+//       moon.classList.remove('active');
+//       sun1.classList.remove('hide');
+//       sun1.classList.add('active');
+//       graph.classList.add('border1');
+//       console.log("dark");
+//       paperBgColor = 'dark';
+//       fontColor = '#aeb5bc';
+//     } else if (userMode === 2) {
+//       // User mode is 2
+//       var htmlElement = document.querySelector('html[data-bs-theme]');
+//       htmlElement.dataset.bsTheme = 'light';
+//       moon.classList.remove('hide');
+//       moon.classList.add('active');
+//       sun1.classList.add('hide');
+//       sun1.classList.remove('active');
+//       graph.classList.add('border');
+//       console.log("light");
+//       paperBgColor = 'light';
+//       fontColor = '#515151';
+//     } else {
+//       // User mode is neither 1 nor 2
+//       console.log("nothing");
+//     }
+//     if (currentTheme !== paperBgColor) {
+//         // Change the theme only if it's different from the user's preference
+//         htmlElement.dataset.bsTheme = paperBgColor;
+//       }
+//     updateGraphColors(paperBgColor, fontColor);
+//   })
+//   .catch(error => {
+//     console.error('Error:', error);
+//   });
 
-  function updateGraphColors(paperBgColor, fontColor) {
-    // Update the graph's background color
-    var layout = {
-      xaxis: { title: "<span style='font-weight: bold; color: " + fontColor + "'>Total Alerts:</span> <span style='color: " + fontColor + "'> " + total + "</span>", tickfont: { color: fontColor } },
-      yaxis: { tickfont: { color: fontColor } },
-      title: "<span style='color: " + fontColor + "'>Weekly Alert Total</span><br>" + "<span style='font-size: 12px;color: " + fontColor + "'>" + sun + " - " + sat + "</span>",
-      paper_bgcolor: paperBgColor,
-      plot_bgcolor: paperBgColor
-    };
+//   function updateGraphColors(paperBgColor, fontColor) {
+//     // Update the graph's background color
+//     var layout = {
+//       xaxis: { title: "<span style='font-weight: bold; color: " + fontColor + "'>Total Alerts:</span> <span style='color: " + fontColor + "'> " + total + "</span>", tickfont: { color: fontColor } },
+//       yaxis: { tickfont: { color: fontColor } },
+//       title: "<span style='color: " + fontColor + "'>Weekly Alert Total</span><br>" + "<span style='font-size: 12px;color: " + fontColor + "'>" + sun + " - " + sat + "</span>",
+//       paper_bgcolor: paperBgColor,
+//       plot_bgcolor: paperBgColor
+//     };
   
-    Plotly.react('barGraph', data, layout);
-  }
+//     Plotly.react('barGraph', data, layout);
+//   }
 
   
